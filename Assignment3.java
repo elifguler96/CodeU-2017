de8e7b2fb8bfdc9909ffd9a33f1ca75fe7f973f3
Elif Güler <elgu96@gmail.com>
2017-06-13 15:55:22 +0300

    Make some refactoring and add comments

--- Assignment3.java
+++ Assignment3.java
@@ -16,11 +16,11 @@ public class Assignment3 {
         boolean[][] visited = new boolean[rowNumber][columnNumber];
         String s = "";
 
-        // Sends each character's row and column numbers to findWordsStartingWithSpecificChar
+        // Sends each character's row and column numbers to formWords
         // and resets the visited array afterwards
         for (int i = 0; i < rowNumber; i++) {
             for (int j = 0; j < columnNumber; j++) {
-                findWordsStartingWithSpecificChar(grid, i, j, visited, s, dictionary);
+                formWords(grid, i, j, visited, s, dictionary);
                 visited = new boolean[rowNumber][columnNumber];
             }
         }
@@ -32,15 +32,16 @@ public class Assignment3 {
     }
 
     /**
-    * Helper method for  
+    * Forms the words in s using recursion
     *
-    * @param  rowNumber    number of rows in the grid
-    * @param  columnNumber number of columns in the grid
-    * @param  grid         gird of characters
-    * @param  dictionary   dictionary containing the words
-    * @return              the list of words found in the grid, null if no words found
+    * @param  grid       gird of characters
+    * @param  row        number of rows in the grid
+    * @param  column     number of columns in the grid
+    * @param  visited    holds whether a character is visited
+    * @param  s          forms the word
+    * @param  dictionary dictionary containing the words
     */
-    private void findWordsStartingWithSpecificChar(char[][] grid, int row, int column, boolean[][] visited, String s,
+    private void formWords(char[][] grid, int row, int column, boolean[][] visited, String s,
                                                    MyDictionary dictionary) {
         // If the new s is not a prefix, we don't have to keep looking
         if (!dictionary.isPrefix(s + grid[row][column])) {
@@ -58,7 +59,7 @@ public class Assignment3 {
         for (int i = row - 1; i <= row + 1 && i < grid.length; i++) {
             for (int j = column - 1; j <= column + 1 && j < grid[0].length; j++) {
                 if (i >= 0 && j >= 0 && !visited[i][j]) {
-                    findWordsStartingWithSpecificChar(grid, i, j, visited, s, dictionary);
+                    formWords(grid, i, j, visited, s, dictionary);
                     visited[i][j] = false;
                 }
             }
