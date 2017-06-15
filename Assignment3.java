260cde7bc8300de8f092f5e2b635e8e783ad4854
Elif Güler <elgu96@gmail.com>
2017-06-15 23:16:47 +0300

    Change imports

--- Assignment3.java
+++ Assignment3.java
@@ -1,4 +1,7 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Assignment3 {
     private Set<String> setOfWords = new HashSet<>();
